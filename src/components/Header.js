1834eef02f13a71f0a543be4983b36f419fef7a6
unknown <prashanthkhiremat@gmail.com>
2024-11-10 14:28:13 +0530

    Episode 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,28 +8,28 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+    <div className="flex justify-between shadow-lg m-2 sm:bg-yellow-200 lg:bg-white">
+      <div className="logo-container flex self-center">
+        <img className="w-20" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">
             Online Status:{onlineStatus} {onlineStatus ? "🟢" : "🔴"}
           </li>
-          <li>
+          <li className="px-4">
             <Link to={"/"}>Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to={"/about"}>About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to={"/contact"}>Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to={"/grocery"}>Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,9 +10,9 @@ const RestaurantCard = ({
   areaName,
 }) => {
   return (
-    <div className="res-card">
-      <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
-      <h3>{name}</h3>
+    <div className="m-4 p-4 w-[250px] bg-gray-100 rounded-lg hover:bg-gray-400">
+      <img className="rounded-lg" src={CDN_URL + cloudinaryImageId} />
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(",")}</h4>
       <h4>{costForTwo}</h4>
       <h4>{avgRating} stars</h4>
